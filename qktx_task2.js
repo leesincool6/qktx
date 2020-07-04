c495d8410906784fd785494ced5ab2e05fa3aaaf
zmasa <65679922+zmmmasa@users.noreply.github.com>
2020-07-04 12:42:25 +0800

    Update qktx_task2.js

--- qktx_task2.js
+++ qktx_task2.js
@@ -1,36 +1,36 @@
 /*
-我的趣看天下邀请码:9849555
-by红鲤鱼与绿鲤鱼与驴 2020.7.3
+//我的趣看天下邀请码:9849555
+//by红鲤鱼与绿鲤鱼与驴 2020.7.3
 
-#趣看天下签到定时执行任务loon定时格式参考
-#趣看天下task Loon的格式
-cron "0 */2 0-22 * * *" script-path=https://raw.githubusercontent.com/zmmmasa/qktx/master/qktx_task2.js, tag=趣看天下
+//#趣看天下签到定时执行任务loon定时格式参考
+//#趣看天下task Loon的格式
+//cron "0 */2 0-22 * * *" script-path=https://raw.githubusercontent.com/zmmmasa/qktx/master/qktx_task2.js, tag=趣看天下
 
-Qx的参考app例子,有不懂的加微信撸金币群。
+//Qx的参考app例子,有不懂的加微信撸金币群。
 
-2020.7.4更新，重大bug更新2
-提示:
-1.获取邀请好友界面开宝箱cookie
-2.每日任务界面的开宝箱cookie
-3.浏览一篇文章小圆圈加载完毕获取阅读cookie
-4.昵称ck
-5.一定运行间隔在1-2分钟以上.
+//2020.7.4更新，重大bug更新2
+//提示:
+//1.获取邀请好友界面开宝箱cookie
+//2.每日任务界面的开宝箱cookie
+//3.浏览一篇文章小圆圈加载完毕获取阅读cookie
+//4.昵称ck
+//5.一定运行间隔在1-2分钟以上.
 
-个别获取ck不全只要在金币记录可以查到金币就好，不要在意界面。
+//个别获取ck不全只要在金币记录可以查到金币就好，不要在意界面。
 
-获取ck完毕可以禁止该js
-#趣看天下ck Qx
-https:\/\/(appv8\.qukantianxia\.com|appv7\.qukantx\.com) url script-request-header https://raw.githubusercontent.com/zmmmasa/qktx/master/qktx_cookie.js
+//获取ck完毕可以禁止该js
+//#趣看天下ck Qx
+//https:\/\/(appv8\.qukantianxia\.com|appv7\.qukantx\.com) url script-request-header https://raw.githubusercontent.com/zmmmasa/qktx/master/qktx_cookie.js
 
-#趣看天下获取ck loon
+//#趣看天下获取ck loon
 
-http-request https:\/\/(appv8\.qukantianxia\.com|appv7\.qukantx\.com) script-path=https://raw.githubusercontent.com/zmmmasa/qktx/master/qktx_cookie.js, requires-body=true, timeout=30, tag=趣看天下ck
+//http-request https:\/\/(appv8\.qukantianxia\.com|appv7\.qukantx\.com) script-path=https://raw.githubusercontent.com/zmmmasa/qktx/master/qktx_cookie.js, requires-body=true, timeout=30, tag=趣看天下ck
 
 ====================================
 
-MITM=appv8.qukantianxia.com,appv7.qukantx.com
+//MITM=appv8.qukantianxia.com,appv7.qukantx.com
 
-*/
+////
 //以上使用说明
 
 
